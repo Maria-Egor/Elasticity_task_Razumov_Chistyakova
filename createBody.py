57f1f0bdbad6f7642d4f492b04a667a217d9de3a
9073ThTa <93887969+9073ThTa@users.noreply.github.com>
2025-01-16 14:45:21 +0300

    fix createBody.py

--- createBody.py
+++ createBody.py
@@ -4,13 +4,13 @@ import math
 def create_Body(n):
     coord_x = []  # массив из координат точек
     coord_y = []  # массив из координат точек
-    x_c = 2  # положение центра кольца
-    y_c = 2
+    x_c = 1  # положение центра кольца
+    y_c = 1
     for r in range(3):
         for i in range(n):
             alpha = 2 * math.pi * i / n #угол
-            x1 = x_c + (1 + r * 0.5) * math.sin(alpha)
-            y1 = y_c + (1 + r * 0.5) * math.cos(alpha)
+            x1 = x_c + (1 + r * 0.5) * math.cos(alpha)
+            y1 = y_c + (1 + r * 0.5) * math.sin(alpha)
 
             coord_x.append(x1)
             coord_y.append(y1)
