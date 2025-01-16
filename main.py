40dedd945a21bbf6cb0e5f30f8f1292904869004
9073ThTa <93887969+9073ThTa@users.noreply.github.com>
2025-01-16 15:04:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from createBody import create_Body
+from create&moveBody import create_Body, move
 import matplotlib.pyplot as plt
 
 x,y= create_Body(24)
 plt.plot(x,y)
 plt.show()
+move(0.1, 1, 50, x, y)
