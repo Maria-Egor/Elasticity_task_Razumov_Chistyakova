859952db6dae3d652565c5e2af1d84f72a3de5c6
Maria Chistyakova <128255183+MariaChist@users.noreply.github.com>
2025-01-16 17:44:43 +0300

    Update main.py
    Добавление вызова функции построения линий тока

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import methods
 import matplotlib.pyplot as plt
+import numpy as np
 
 x,y = methods.create_ring(24)
 plt.plot(x,y,'.')
 plt.grid(color='gray', linestyle='--', linewidth=0.5)
 plt.show()
 methods.movets(0.1, 1, 50, x, y)
+
+for t in np.arange(0.1, 1.1, 0.1):
+    methods.plot_streamlines(t, 10, 10)
