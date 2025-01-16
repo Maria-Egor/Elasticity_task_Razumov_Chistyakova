60ce279382cf0aa2fdca939b63ffe09ae9c29560
Maria Chistyakova <128255183+MariaChist@users.noreply.github.com>
2025-01-16 13:42:12 +0300

    Файл, "создающий" исследуемое тело
    Возвращает два массива из координат точек кольца с радиусами 1 и 2 и центром в точке (2;2), принадлежащие радиусу в 2,1,5 и 1
    На вход получает число точек n в одном из трех "колец", то есть результат - массивы длинной 3*m

---
+++ createBody.py
@@ -0,0 +1,24 @@
+import math
+
+
+def create_Body(n):
+    coord_x = []  # массив из координат точек
+    coord_y = []  # массив из координат точек
+    x_c = 2  # положение центра кольца
+    y_c = 2
+    for r in range(3):
+        for i in range(n):
+            alpha = 2 * math.pi * i / n #угол
+            x1 = x_c + (1 + r * 0.5) * math.sin(alpha)
+            y1 = y_c + (1 + r * 0.5) * math.cos(alpha)
+
+            coord_x.append(x1)
+            coord_y.append(y1)
+    return coord_x, coord_y
+
+""" проверка работы кода
+import matplotlib.pyplot as plt
+
+x, y = create_Body(24)
+plt.plot(x, y,'o')
+plt.show()"""
