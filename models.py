418a3d64c39bee87113f74a6f3b3c04b07128ffa
Maria Chistyakova <128255183+MariaChist@users.noreply.github.com>
2025-01-16 15:40:20 +0300

    Update models.py
    Добавление инициализации и add

--- models.py
+++ models.py
@@ -1,3 +1,15 @@
 class Trajectory:
     x_tr_points = []
     y_tr_points = []
+    
+    def __init__(self, x_tr_points, y_tr_points):
+        self.x_tr_points = x_tr_points
+        self.y_tr_points = y_tr_points
+
+    def __init__(self):
+        self.x_tr_points = []
+        self.y_tr_points = []
+
+    def add(self, x, y):
+        self.x_tr_points.append(x)
+        self.y_tr_points.append(y)
