483ae465c79a10c3b9da5a19eebb4dac2bc0a8a8
9073ThTa <93887969+9073ThTa@users.noreply.github.com>
2025-01-16 16:43:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from create_moveBody import create_Body, move
+from plotTraj import movets
 import matplotlib.pyplot as plt
 
 x,y= create_Body(24)
 plt.plot(x,y)
 plt.show()
-move(0.1, 1, 50, x, y)
+movets(0.1, 1, 50, x, y)
