c8aa7fac3d4b033edaab01afaa5a5d95d0bf9a33
Maria Chistyakova <128255183+MariaChist@users.noreply.github.com>
2025-01-17 16:18:51 +0300

    Update main.py
    Выравнивание масштаба графиков

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 x,y = methods.create_ring(24)
 plt.plot(x,y,'.')
 plt.grid(color='gray', linestyle='--', linewidth=0.5)
+plt.axis('equal')
 plt.show()
 [x_end,y_end]=methods.movets(0.1, 1, 50, x, y)
 methods.deformation(x, y, x_end, y_end)
