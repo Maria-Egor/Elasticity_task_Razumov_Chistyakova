f72a63f0fbf55a979e8a05dc250dcd1a4ff066d8
Maria Chistyakova <128255183+MariaChist@users.noreply.github.com>
2025-01-20 18:29:19 +0300

    Update models.py
    Удаление неиспользуемых функций

--- models.py
+++ models.py
@@ -2,13 +2,7 @@ import numpy as np
 
 
 class Trajectory:
-    x_tr_points = []
-    y_tr_points = []
     
-    def __init__(self, x_tr_points, y_tr_points):
-        self.x_tr_points = x_tr_points
-        self.y_tr_points = y_tr_points
-
     def __init__(self):
         self.x_tr_points = []
         self.y_tr_points = []
